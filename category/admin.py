8e78d2edfb504a3cd9e913b63855088a66fc81ad
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-06 01:08:28 +0600

    [CREATED] Superuser created

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from category.models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description')
+
+
+admin.site.register(Category, CategoryAdmin)
