faf1aef89d135ba7f871df8ad655988961add438
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-06 23:24:55 +0600

    [CREATED] Custom user model cerated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'first_name', 'last_name',)
+
+
+admin.site.register(Account, AccountAdmin)
