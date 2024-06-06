c14c6db0e927e5c23d8e36f5692cd1d4a2905c8d
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-06 23:55:01 +0600

    [UPDATED] Update password filed is read only and make clicable links email and first name

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'username', 'first_name', 'last_name',)
-
+class AccountAdmin(UserAdmin):
+    list_display = ('id' ,'email',  'first_name', 'last_name', 'username', 'phone', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
 
 admin.site.register(Account, AccountAdmin)
