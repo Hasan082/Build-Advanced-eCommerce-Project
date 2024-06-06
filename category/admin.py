57bacad7255608aeaba4cf70ea8fb98b75e901cc
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-07 01:04:27 +0600

    [ADDED] Added comment to Category Model

--- category/admin.py
+++ category/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Category
 
 
+# Defining the CategoryAdmin class to customize the admin interface for the Category model.
 class CategoryAdmin(admin.ModelAdmin):
+    # list_display specifies the fields to display in the list view of the Category model.
     list_display = ('name', 'slug', 'description')
 
 
+# Registering the Category model with the admin site using the CategoryAdmin class.
 admin.site.register(Category, CategoryAdmin)
