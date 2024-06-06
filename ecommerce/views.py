e42dc1c02fed69cc2cf79807ed99d17683cc791a
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-07 01:05:39 +0600

    [ADDED] Added comment to Views file

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render  # Importing the render function to render templates.
 
 
+# Defining the index view function which handles requests to the home page.
 def index(request):
+    # The render function takes the request object, the template name, and an optional context dictionary.
     return render(request, 'index.html')
