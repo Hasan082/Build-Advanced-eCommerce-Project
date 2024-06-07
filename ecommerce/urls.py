394d58382ae9890f19123910c13c2caa7afe88f8
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-07 18:20:57 +0600

    [ADDED] Load all html in store page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
