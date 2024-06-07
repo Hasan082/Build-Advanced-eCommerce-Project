d2b503d07afa30dbd609ff93ac505632c2b512a8
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-07 17:30:00 +0600

    [ADDED] Show all product in homepage

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render  # Importing the render function to render templates.
+from store.models import Product
 
 
 # Defining the index view function which handles requests to the home page.
 def index(request):
+    # Get all product from Store with filter available
+    products = Product.objects.all().filter(is_available=True)
+    context = {'products': products}
     # The render function takes the request object, the template name, and an optional context dictionary.
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
