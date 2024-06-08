ef29e78305a44c358d9e63d5b109846818823f66
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-08 21:28:26 +0600

    [ADDED] Banner Image and store page url fixed

--- store/models.py
+++ store/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 
 class Product(models.Model):
-    prod_name       = models.CharField(max_length=255, unique=True)
-    slug            = models.SlugField(max_length=255, unique=True)
-    description     = models.TextField(max_length=500, blank=True)
-    price           = models.DecimalField(max_digits=5, decimal_places=2)
-    image           = models.ImageField(upload_to='photos/products/')
-    stock           = models.PositiveIntegerField(default=0)
-    is_available    = models.BooleanField(default=True)
-    category        = models.ForeignKey(Category, on_delete=models.CASCADE)
-    created_at      = models.DateTimeField(auto_now_add=True)
-    updated_at      = models.DateTimeField(auto_now=True)
+    prod_name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='photos/products/')
+    stock = models.PositiveIntegerField(default=0)
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('product_details', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.prod_name
