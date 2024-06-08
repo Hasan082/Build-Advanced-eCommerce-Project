19e930c274d6e4436152f7b7fcbed0515281a627
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-09 01:24:48 +0600

    [ADDED] Cart views function added

--- carts/models.py
+++ carts/models.py
@@ -3,11 +3,11 @@ from store.models import Product
 
 
 class Cart(models.Model):
-    card_id = models.CharField(max_length=250, blank=True)
+    cart_id = models.CharField(max_length=250, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.card_id
+        return self.cart_id
 
 
 class cartItem(models.Model):
--- carts/urls.py
+++ carts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart_home, name='cart_home'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
 ]
