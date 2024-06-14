abf9ac3e22b342870eb311b77ef83dd19b5ffa16
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-14 16:51:32 +0600

    [ADDED] Remove, decrease cart item function added

--- carts/urls.py
+++ carts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cart_home, name='cart_home'),
     path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
+    path('delete_cart/<int:product_id>/', views.delete_cart, name='delete_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from carts.models import Cart, cartItem
 from store.models import Product
@@ -38,6 +37,27 @@ def add_cart(request, product_id):
     return redirect('cart_home')
 
 
+def remove_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    cart_item = get_object_or_404(cartItem, product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+
+    return redirect('cart_home')
+
+
+def delete_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    cart_item = get_object_or_404(cartItem, product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart_home')
+
+
 def cart_home(request, total=0, quantity=0, cart_items=None):
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -45,12 +65,16 @@ def cart_home(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
     except ObjectDoesNotExist:
-        pass # just ignore
+        pass  # just ignore
 
     context = {
         'total': total,
         'quantity': quantity,
-        'cart_items': cart_items
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total
     }
     return render(request, 'cart.html', context)
