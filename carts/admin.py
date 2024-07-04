e3653f72ed764dfc215081eb1fe8a95f84c9b327
Md Hasanuzzaman <dr.has82@gmail.com>
2024-07-04 22:59:47 +0600

    [CART] commnet added for cart model

--- carts/admin.py
+++ carts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Cart, cartItem
 
+# Register the Cart model with the admin site.
+# This allows the Cart model to be managed through the Django admin interface.
 admin.site.register(Cart)
+
+# Register the cartItem model with the admin site.
+# This allows the cartItem model to be managed through the Django admin interface.
 admin.site.register(cartItem)
