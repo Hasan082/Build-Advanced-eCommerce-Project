355966cdb22e690f4bcdbef36542aac5e021ae29
Md Hasanuzzaman <dr.has82@gmail.com>
2024-07-11 12:11:32 +0600

    [AUTH] User registraion password and confirm password validation done

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,6 +12,13 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords does not match")
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         placeholders = {
