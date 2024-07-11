860c3eabff3eac4b58bf0b3bfd2f1783b2c3ca86
Md Hasanuzzaman <dr.has82@gmail.com>
2024-07-11 13:30:36 +0600

    [AUTH] User Login successfully and show error message done

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,25 @@
-from django.contrib import messages
-
+from django.contrib import messages,auth
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
 
 
-# Create your views here.
+# USER REGISTRATION VIEWs
 def register(request):
+    """
+        Handle user registration.
+
+        Validates the registration form submitted via POST request.
+        If valid, creates a new user account and redirects to registration page with success message.
+        If invalid, redirects back to registration page with error messages displayed.
+
+        Args:
+        - request: HTTP request object
+
+        Returns:
+        - HTTP response redirecting to registration page
+
+        """
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -38,7 +51,20 @@ def register(request):
     context = {'form': form}
     return render(request, 'register.html', context)
 
+
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid email or password.')
+            return redirect('login')
     return render(request, 'login.html')
 
 
